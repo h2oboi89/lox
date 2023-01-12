e482145f9c1cdb1d0ed2027bc9221c4da840854f
Waters <h2oboi89@gmail.com>
2023-01-11 20:40:26 -0500

    extracted classes to own files

---
+++ Interpreter.Framework/Scanning/Token.cs
@@ -0,0 +1,6 @@
+﻿namespace Interpreter.Framework.Scanning;
+
+public readonly record struct Token(TokenType Type, string Lexeme, int Line, object? Literal = null)
+{
+    public override string ToString() => $"{Line} {Type} {Lexeme} {Literal}";
+}
