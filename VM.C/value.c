80f1259275bf18309836b6b58245d0ca2e26b4a3
Waters <h2oboi89@gmail.com>
2023-01-16 15:25:48 -0500

    finished ch18

--- VM.C/value.c
+++ VM.C/value.c
@@ -22,10 +22,28 @@ void writeValueArray(ValueArray* array, Value value) {
 }
 
 void freeValueArray(ValueArray* array) {
-    FREE_ARRAY(Value, array->values , array->capacity);
+    FREE_ARRAY(Value, array->values, array->capacity);
     initValueArray(array);
 }
 
 void printValue(Value value) {
-    printf("%g", value);
+    switch (value.type)
+    {
+    case VAL_BOOL: printf(AS_BOOL(value) ? "true" : "false"); break;
+    case VAL_NIL: printf("nil"); break;
+    case VAL_NUMBER: printf("%g", AS_NUMBER(value)); break;
+    default: break;
+    }
+}
+
+bool valuesEqual(Value a, Value b) {
+    if (a.type != b.type) return false;
+
+    switch (a.type)
+    {
+    case VAL_BOOL:      return AS_BOOL(a) == AS_BOOL(b);
+    case VAL_NIL:       return true;
+    case VAL_NUMBER:    return AS_NUMBER(a) == AS_NUMBER(b);
+    default:            return false;
+    }
 }
