261a46726c62d82480e816cd7e16d15ecb318ecd
Waters <h2oboi89@gmail.com>
2023-01-19 21:19:38 -0500

    function calls

--- VM.C/vm.c
+++ VM.C/vm.c
@@ -23,10 +23,20 @@ static void runtimeError(const char* format, ...) {
     va_end(args);
     fputs("\n", stderr);
 
-    CallFrame* frame = &vm.frames[vm.frameCount - 1];
-    size_t instruction = frame->ip - frame->function->chunk.code - 1;
-    int line = frame->function->chunk.lines[instruction];
-    fprintf(stderr, "[line %d] in script\n", line);
+    for (int i = vm.frameCount - 1; i >= 0; i--) {
+        CallFrame* frame = &vm.frames[i];
+        ObjectFunction* function = frame->function;
+        size_t instruction = frame->ip - function->chunk.code - 1;
+
+        fprintf(stderr, "[line %d] in ", function->chunk.lines[instruction]);
+        if (function->name == NULL) {
+            fprintf(stderr, "script\n");
+        }
+        else {
+            fprintf(stderr, "%s()\n", function->name->chars);
+        }
+    }
+
     resetStack();
 }
 
@@ -55,6 +65,38 @@ Value pop() {
 
 static Value peek(int distance) {
     return vm.stackTop[-1 - distance];
+}
+
+static bool call(ObjectFunction* function, int argCount) {
+    if (argCount != function->arity) {
+        runtimeError("Expected %d arguments but got %d", function->arity, argCount);
+        return false;
+    }
+
+    if (vm.frameCount == FRAMES_MAX) {
+        runtimeError("Stack overflow.");
+        return false;
+    }
+
+    CallFrame* frame = &vm.frames[vm.frameCount++];
+    frame->function = function;
+    frame->ip = function->chunk.code;
+    frame->slots = vm.stackTop - argCount - 1;
+    return true;
+}
+
+static bool callValue(Value callee, int argCount) {
+    if (IS_OBJECT(callee)) {
+        switch (OBJECT_TYPE(callee))
+        {
+        case OBJECT_FUNCTION:
+            return call(AS_FUNCTION(callee), argCount);
+        default:
+            break;
+        }
+    }
+    runtimeError("Can only call functions and classes.");
+    return false;
 }
 
 static bool isFalsey(Value value) {
@@ -110,9 +152,9 @@ static InterpretResult run() {
         disassembleInstruction(&frame->function->chunk, (int)(frame->ip - frame->function->chunk.code));
 #endif // !DEBUG_TRACE_EXECUTION
 
-        uint8_t instruction;
+        uint8_t instruction = READ_BYTE();
 
-        switch (instruction = READ_BYTE())
+        switch (instruction)
         {
         case OP_CONSTANT: {
             Value constant = READ_CONSTANT();
@@ -221,6 +263,15 @@ static InterpretResult run() {
             break;
         }
 
+        case OP_CALL: {
+            int argCount = READ_BYTE();
+            if (!callValue(peek(argCount), argCount)) {
+                return INTERPRET_RUNTIME_ERROR;
+            }
+            frame = &vm.frames[vm.frameCount - 1];
+            break;
+        }
+
         case OP_RETURN: {
             return INTERPRET_OK;
         }
@@ -239,10 +290,7 @@ InterpretResult interpret(const char* source) {
     if (function == NULL) return INTERPRET_COMPILE_ERROR;
 
     push(OBJECT_VAL(function));
-    CallFrame* frame = &vm.frames[vm.frameCount++];
-    frame->function = function;
-    frame->ip = function->chunk.code;
-    frame->slots = vm.stack;
+    call(function, 0);
 
     return run();
 };
