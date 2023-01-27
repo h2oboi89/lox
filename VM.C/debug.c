a48b3c19272ab016d36e8bf291d4cc8e64d61382
Waters <h2oboi89@gmail.com>
2023-01-26 21:36:52 -0500

    get and set

--- VM.C/debug.c
+++ VM.C/debug.c
@@ -74,6 +74,10 @@ int disassembleInstruction(Chunk* chunk, int offset) {
 		return constantInstruction("OP_DEFINE_GLOBAL", chunk, offset);
 	case OP_SET_GLOBAL:
 		return constantInstruction("OP_SET_GLOBAL", chunk, offset);
+	case OP_GET_PROPERTY:
+		return constantInstruction("OP_GET_PROPERTY", chunk, offset);
+	case OP_SET_PROPERTY:
+		return constantInstruction("OP_SET_PROPERTY", chunk, offset);
 	case OP_EQUAL:
 		return simpleInstruction("OP_EQUAL", offset);
 	case OP_GREATER:
